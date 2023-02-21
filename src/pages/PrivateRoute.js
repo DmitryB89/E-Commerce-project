586d9968bf51d5b918450bb5960839638113ca07
Dmitry <d.baydarov@yandex.ru>
2023-02-21 23:20:38 +0300

    navbar sidebar

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 // will remove later
 import { useUserContext } from '../context/user_context';
 
 const PrivateRoute = () => {
-  return <h4>Private Route</h4>;
+  return <div>aaaaa</div>
+
+  // if(!user) {
+  //   return < Navigate to={'/'}/>
+  // }
+  // return children
 };
 export default PrivateRoute;
