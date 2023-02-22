328737383f3cbe2a0e3efced12c1cc016af7d7db
Dmitry <d.baydarov@yandex.ru>
2023-02-22 23:33:10 +0300

    Product image, single product

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+    return new Intl.NumberFormat('ru', {
+        style: "currency",
+        currency: 'RUB'
+    }).format(number / 100)
+}
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {
+}
